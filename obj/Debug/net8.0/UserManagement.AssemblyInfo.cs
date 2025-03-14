1cee459c60aa0c17571dc8491a552873c83bd9d5
e0046704 <e0046704@u.nus.edu>
2025-03-14 22:43:34 +0800

    Test Commit

--- obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+febbcd5ae59093d3a666c54b55e371d49adb7493")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

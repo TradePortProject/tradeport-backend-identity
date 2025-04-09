71eed9a81464938dfb9e1e5edfce65552d7e7b1c
sree <sreeraj_ec@hotmail.com>
2025-04-09 08:40:39 +0800

    SCRUM-161-Adding-Snyk

--- obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/UserManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc37eb1bfede393ea7d4c24843dd93a110f62b31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e5909531c0486937b2433388c7f7265a2a8917")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

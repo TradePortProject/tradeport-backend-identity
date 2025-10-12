03afcdf24b477dcb73d1b1a89d736c63cb75af96
sumon <e0046476@u.nus.edu>
2025-10-12 14:54:23 +0800

    Moved the health check to controller

--- UserManagement/Controllers/UserController.cs
+++ UserManagement/Controllers/UserController.cs
@@ -172,6 +172,13 @@ namespace UserManagement.Controllers
             }
         }
 
+        [HttpGet("health")]          
+        public IActionResult Health() => Ok(new { status = "ok" });
+
+        [HttpGet("ready")]      
+        public IActionResult Ready() => Ok(new { status = "ready" });
+
+
     }
 
 
